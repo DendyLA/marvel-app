9accd1274830ad86ff56ca7d0e54c79eebfaed0a
Dawa <atayewdawlet@gmail.com>
2024-02-02 18:48:20 +0200

    Fetch with api function and randomChar component is done

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -2,7 +2,7 @@
 
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=f1d745a6afa90b36aac7253bf870a85a';
+    _apiKey = 'ts=1&apikey=f1d745a6afa90b36aac7253bf870a85a&hash=07dbd3fa3d8501d00774827557385195';
 
     getResources = async (url) => {
         let res = await fetch(url);
@@ -13,13 +13,27 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = () => {
-        return this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    getAllCharacters = async () => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
 
-    getCharacter = (id) => {
-        return this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    getCharacter = async (id) => {
+        const res = await this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return{
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
+
+
 
 export default MarvelService
